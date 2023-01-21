b64e8215554bd46482debeebfe151e48af81a599
jonwyun <113542871+jonwyun@users.noreply.github.com>
2023-01-21 18:42:15 -0500

    completing files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+console.log('Javascript is running!');
+
+const oneGraphic = document.querySelector("#icon_one");
+const twoGraphic = document.querySelector("#icon_two");
+const threeGraphic = document.querySelector("#icon_three");
+const fourGraphic = document.querySelector("#icon_four");
+const fiveGraphic = document.querySelector("#icon_five");
+
+const nineGraphic = document.querySelector("#nineIconImage");
+const tenGraphic = document.querySelector("#tenIconImage");
+// const nineGraphic = document.querySelector("#icon_six");
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+oneGraphic.addEventListener('click', logThisId);
+twoGraphic.addEventListener('click', logThisId);
+threeGraphic.addEventListener('click', logThisId);
+fourGraphic.addEventListener('click', logThisId);
+fiveGraphic.addEventListener('click', logThisId);
+
+nineGraphic.addEventListener('click', logThisId);
+tenGraphic.addEventListener('click', logThisId);
+// sixGraphic.addEventListener('click', logThisId);
